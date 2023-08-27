5c17075553fdeb81081c6fce2cec274b04f51db7
margohpolo <mark.g.goh@gmail.com>
2023-08-27 22:20:45 +0800

    shifted tile & board to separate working files

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -1,48 +1,7 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace TicTacToe
 {
-    public class Tile
-    {
-        public int X {get; set;}
-        public int Y {get; set;}
-        public char Symbol {get; set;}
-    }
-
-    public class Board
-    {
-       private List<Tile> _plays = new List<Tile>();
-       
-        public Board()
-        {
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    _plays.Add(new Tile{ X = i, Y = j, Symbol = ' '});
-                }  
-            }       
-        }
-       public Tile TileAt(int x, int y)
-       {
-           return _plays.Single(tile => tile.X == x && tile.Y == y);
-       }
-
-       public void AddTileAt(char symbol, int x, int y)
-       {
-           var newTile = new Tile
-           {
-               X = x,
-               Y = y,
-               Symbol = symbol
-           };
-
-           _plays.Single(tile => tile.X == x && tile.Y == y).Symbol = symbol;
-       }
-    }
-
     public class Game
     {
         private char _lastSymbol = ' ';
---
+++ TicTacToe/Tile.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TicTacToe
+{
+    public class Tile
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Symbol { get; set; }
+    }
+}
